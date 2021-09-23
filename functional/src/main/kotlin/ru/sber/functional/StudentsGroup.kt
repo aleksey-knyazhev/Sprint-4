8bd569df9e2408d014453a606a02ccf0613cd885
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-09-23 18:06:52 +0300

    functional

--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -1,10 +1,17 @@
 package ru.sber.functional
 
 class StudentsGroup {
+    private var students: List<Student> = listOf(
+        Student(firstName = "Иван", lastName = "Иванов", averageRate = 5.0),
+        Student(firstName = "Петр", lastName = "Петров", averageRate = 4.5, middleName = "Петрович"),
+        Student(firstName = "Сидор", lastName = "Сидоров", averageRate = 4.0, age = 42),
+    )
 
-    lateinit var students: List<Student>
+    fun filterByPredicate(predicate: (Student) -> Boolean): List<Student> = students.filter { predicate(it) }
+}
 
-    fun filterByPredicate() {
-        TODO()
-    }
+fun main() {
+    val studentsGroup = StudentsGroup().filterByPredicate { it.averageRate >= 4.5 }
+    for (student in studentsGroup)
+        println("${student.firstName} ${student.lastName} ${student.averageRate}")
 }
