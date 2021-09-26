078fa1437211876fd776cbef50eea84dde2155d9
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-09-27 00:17:53 +0300

    Sorter

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,24 +1,32 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
 import java.util.*
 
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <K, V> compare(p1: Pair<K, V>, p2: Pair<K, V>): Boolean {
+    return p1 == p2
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <T: Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T): Int {
+    var result = 0
+
+    for (i in anArray) {
+        if (i > elem)
+            result++
+    }
+
+    return result
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<E: Comparable<E>> {
+    val list: MutableList<E> = mutableListOf()
+
+    fun add(value: E) {
+        list.add(value)
+        list.sort()
+    }
 }
 
 // 4.
